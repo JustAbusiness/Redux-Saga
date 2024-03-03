e18b0414b8700add56c7b385705ef9b402a4c412
JustAbusiness <huysanti123456@gmail.com>
2024-03-03 16:35:03 +0700

    first push

---
+++ src/redux/counter/counter.slide.ts
@@ -0,0 +1,33 @@
+import { createSlice } from '@reduxjs/toolkit'
+import type { RootState } from '../store'
+
+// Define a type for the slice state
+export interface CounterState {
+  value: number
+}
+
+// Define the initial state using that type
+const initialState: CounterState = {
+  value: 11,
+  name: "Huy",
+  address: {
+    city: "HCM"
+  }
+}
+
+export const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    increment: state => {
+      state.value += 1
+    },
+  }
+})
+
+export const { increment} = counterSlice.actions
+
+// Other code such as selectors can use the imported `RootState` type
+export const selectCount = (state: RootState) => state.count.value
+
+export default counterSlice.reducer
