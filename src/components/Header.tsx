89ad8f4ad8a69ed4bdf7ebe95b85c06d862a07f8
JustAbusiness <huysanti123456@gmail.com>
2024-03-23 08:02:16 +0700

    dfd

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,34 @@
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
+import Form from "react-bootstrap/Form";
+import { useState } from "react";
 
 function Header() {
+  const [mode, setMode] = useState<string>("light");
+
+
   return (
     <div>
-      <Navbar className="bg-body-tertiary mb-2">
-      <Container>
-        <Navbar.Brand href="#home">Navbar with text</Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text>
-            Signed in as: <a href="#login">Mark Otto</a>
-          </Navbar.Text>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+      <Navbar className="bg-body-tertiary mb-2" data-bs-theme={mode}>
+        <Container>
+          <Navbar.Brand href="#home">Navbar with text</Navbar.Brand>
+          <Navbar.Toggle />
+          <Form>
+            <Form.Check
+              value={mode}
+              onChange={() => setMode(mode === "light" ? "dark" : "light")}
+              type="switch"
+              id="custom-switch"
+              label={ mode === "light" ? <Navbar.Text>Light Mode </Navbar.Text> : <Navbar.Text>Dark Mode </Navbar.Text>}
+            />
+          </Form>
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text>
+              Signed in as: <a href="#login">Mark Otto</a>
+            </Navbar.Text>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
   );
 }
--- src/redux/user/user.slide.ts
+++ src/redux/user/user.slide.ts
@@ -4,50 +4,85 @@ import type { RootState } from "../store";
 interface IUser {
   id: number;
   name: string;
-  email: string
+  email: string;
 }
 
 export const fecthListUsers = createAsyncThunk(
-  'users/fetchListUsers',
+  "users/fetchListUsers",
   async (userId, thunkApi) => {
-    const response = await fetch('http://localhost:8000/users');
+    const response = await fetch("http://localhost:8000/users");
     const data = await response.json();
-    return data
+    return data;
   }
-)
+);
 
 interface IUserPayload {
-  email: string,
-  name: string  
+  email: string;
+  name: string;
 }
 
 export const createNewUser = createAsyncThunk(
-  'users/createNewUser',
-  async (payload:IUserPayload, thunkApi) => {
-    const response = await fetch('http://localhost:8000/users', {
-      method: 'POST',
+  "users/createNewUser",
+  async (payload: IUserPayload, thunkApi) => {
+    const response = await fetch("http://localhost:8000/users", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({ ...payload }),
     });
 
     const data = await response.json();
-    console.log(data)
+    console.log(data);
     if (data & data.id) {
       thunkApi.dispatch(fecthListUsers());
     }
-    return data
+    return data;
   }
-)
+);
 
+export const updateAUser = createAsyncThunk(
+  "users/updateAUser",
+  async (payload: IUserPayload, thunkApi) => {
+    const response = await fetch(`http://localhost:8000/users/${payload.id}`, {
+      method: "PUT",
+      body: JSON.stringify({
+        email: payload.email,
+        name: payload.name,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    const data = await response.json();
+    if (data & data.id) {
+      thunkApi.dispatch(fecthListUsers());
+    }
+    return data;
+  }
+);
+
+export const deleteUser = createAsyncThunk(
+  "users/deleteUser",
+  async (payload: any, thunkApi) => {
+    const response = await fetch(`http://localhost:8000/users/${payload.id}`, {
+      method: "DELETE",
+    });
+    const data = await response.json();
+    thunkApi.dispatch(fecthListUsers());
+    return data;
+  }
+);
 
 const initialState: {
-  listUsers: IUser[],
-  isCreateSuccess: boolean
+  listUsers: IUser[];
+  isCreateSuccess: boolean;
+  isDeleteSuccess: boolean;
+  isUpdateSuccess: boolean;
 } = {
   listUsers: [],
-  isCreateSuccess: false
+  isCreateSuccess: false,
+  isDeleteSuccess: false,
+  isUpdateSuccess: false,
 };
 
 export const userSlice = createSlice({
@@ -56,17 +91,22 @@ export const userSlice = createSlice({
   reducers: {
     resetCreate(state) {
       state.isCreateSuccess = false;
-    }
+    },
   },
   extraReducers: (builder) => {
-    builder
-      .addCase(fecthListUsers.fulfilled, (state, action) => {
-        state.listUsers = action.payload
-      }),
+    builder.addCase(fecthListUsers.fulfilled, (state, action) => {
+      state.listUsers = action.payload;
+    });
     builder.addCase(createNewUser.fulfilled, (state, action) => {
-      state.isCreateSuccess = true
-    })  
-  }
+      state.isCreateSuccess = true;
+    })
+    .addCase(updateAUser.fulfilled, (state, action) => {
+      state.isUpdateSuccess = true
+    })
+    .addCase(deleteUser.fulfilled, (state, action) => {
+      state.isDeleteSuccess = true
+    })
+  },
 });
 
 export const { resetCreate } = userSlice.actions;
